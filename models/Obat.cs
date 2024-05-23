6e7e8cc368003adc7bf8b7d635e445d6faf687f3
Rayhan <75935943+kienmiiru@users.noreply.github.com>
2024-05-23 18:03:55 +0700

    Menambahkan class Obat (ngetes)

---
+++ models/Obat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectPBO.models
+{
+    public class Obat
+    {
+        public int idObat;
+        public string nama;
+        public string jenis;
+
+        public Obat(int idObat, string nama, string jenis)
+        {
+            this.idObat = idObat;
+            this.nama = nama;
+            this.jenis = jenis;
+        }
+    }
+}
