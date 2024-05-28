63b4486dd391d450ad402cb1f18d0fbdf5cccb3f
SiPemula03 <149750299+SiPemula03@users.noreply.github.com>
2024-05-28 17:09:22 +0700

    punya emirMock up antrian
    Aku buat bagian mock up bagian buat antrian

---
+++ Home_Page_Admin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectPBO
+{
+    public partial class Home_Page_Admin : Form
+    {
+        public Home_Page_Admin()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Home_Page_User.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace ProjectPBO
+{
+    partial class Home_Page_User
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 40F);
+            label1.Location = new Point(337, 33);
+            label1.Name = "label1";
+            label1.Size = new Size(404, 89);
+            label1.TabIndex = 0;
+            label1.Text = "HOME PAGE";
+            label1.Click += label1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.InactiveCaptionText;
+            textBox1.Font = new Font("Segoe UI", 40F);
+            textBox1.ForeColor = SystemColors.InactiveBorder;
+            textBox1.Location = new Point(93, 202);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(380, 232);
+            textBox1.TabIndex = 1;
+            textBox1.Text = "BUAT ANTRIAN";
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = SystemColors.InactiveCaptionText;
+            textBox2.Font = new Font("Segoe UI", 40F);
+            textBox2.ForeColor = SystemColors.InactiveBorder;
+            textBox2.Location = new Point(623, 202);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(415, 232);
+            textBox2.TabIndex = 2;
+            textBox2.Text = "KONSULTASI KESEHATAN";
+            textBox2.TextAlign = HorizontalAlignment.Center;
+            // 
+            // Form3
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1119, 607);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Form3";
+            Text = "Form3";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+    }
+}
