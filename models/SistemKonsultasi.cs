8f84319c1ac03a42ded0793c61346ca092609e6e
Hannsu <mfarhanyanuara@gmail.com>
2024-06-09 13:39:39 +0700

    sistem konsultasi

--- models/SistemKonsultasi.cs
+++ models/SistemKonsultasi.cs
@@ -11,22 +11,31 @@ namespace ProjectPBO.models
         public List<Gejala> daftarGejala;
         public List<Penyakit> daftarPenyakit;
 
-        public SistemKonsultasi()
-        {
-            daftarGejala = new List<Gejala>();
-            daftarPenyakit = new List<Penyakit>();
-        }
-        public void tambahGejala(Gejala gejala)
-        {
-            daftarGejala.Add(gejala);
-        }
-
         public List<(Penyakit, float)> getHasilPrediksi()
         {
             var hasilPrediksi = new List<(Penyakit, float)>();
-            // Belum jadi
-            hasilPrediksi.Add((daftarPenyakit[0], 0.98F));
-            hasilPrediksi.Add((daftarPenyakit[1], 0.451F));
+            foreach (var penyakit in daftarPenyakit)
+            {
+                int cocok = 0;
+                foreach (var gejala in penyakit.listGejala)
+                {
+                    var ada = false;
+                    foreach (var i in daftarGejala)
+                    {
+                        if (i.idGejala == gejala.idGejala)
+                        {
+                            ada = true;
+                            break;
+                        }
+                    }
+                    if (ada)
+                    {
+                        cocok++;
+                    }
+                }
+                hasilPrediksi.Add((penyakit, (float)cocok / penyakit.listGejala.Count));
+            }
+            hasilPrediksi.Sort((x, y) => y.Item2.CompareTo(x.Item2));
             return hasilPrediksi;
         }
     }
