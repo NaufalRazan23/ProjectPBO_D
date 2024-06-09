1d88b951ff1d2345fe714efcfbc8f6e310701a22
wahyufirdani114 <danisugiyono98@gmail.com>
2024-06-09 11:07:55 +0700

    perbaikan tombol

--- popupSelesai.Designer.cs
+++ popupSelesai.Designer.cs
@@ -133,6 +133,7 @@
             button1.TabIndex = 8;
             button1.Text = "OK";
             button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // popupSelesai
             // 
